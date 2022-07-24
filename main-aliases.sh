c86d4aace0b91c562001accb3c5ec40320f70c45
Maxime BOCHON <maxime.bochon@gmail.com>
2022-07-24 14:43:56 +0200

    Add alias to find non-empty duplicates

--- main-aliases.sh
+++ main-aliases.sh
@@ -1,2 +1,5 @@
 # Get information on the process associated to a window by clicking on it
 alias getWindowProcessInformation='echo "Click on a window to get its process information"; ps -p $(xprop | awk '\''/PID/ {print $3}'\'') -o user,pid,args'
+
+# Find non-empty duplicated files, recursively from the current directory
+alias findDuplicatesExcludingEmptyFiles="fdupes -n -R ."
