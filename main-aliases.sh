17897e6c12affce3876af3c09284a4a42a96f3f5
Maxime BOCHON <maxime.bochon@gmail.com>
2022-07-24 14:41:53 +0200

    Add alias description

--- main-aliases.sh
+++ main-aliases.sh
@@ -1,1 +1,2 @@
+# Get information on the process associated to a window by clicking on it
 alias getWindowProcessInformation='echo "Click on a window to get its process information"; ps -p $(xprop | awk '\''/PID/ {print $3}'\'') -o user,pid,args'
