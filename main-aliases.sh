b4844e315237ca73dab5fa2f5721db192681702a
Maxime BOCHON <maxime.bochon@gmail.com>
2022-07-24 14:45:57 +0200

    Add alias to list file extensions recursively

--- main-aliases.sh
+++ main-aliases.sh
@@ -3,3 +3,7 @@ alias getWindowProcessInformation='echo "Click on a window to get its process in
 
 # Find non-empty duplicated files, recursively from the current directory
 alias findDuplicatesExcludingEmptyFiles="fdupes -n -R ."
+
+# List file extensions, from 1 to 5 characters, found recursively from the current directory, ignoring case
+alias listFileExtensionsIgnoreCase="find . -type f -regextype posix-extended -regex '.+\.[^.]{1,5}' | sed --regexp-extended 's/.+\.([^.]{1,5})/\1/g' | tr [:upper:] [:lower:] | sort -u"
+
