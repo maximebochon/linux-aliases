0401880496e4227b433c700a91f5e17aa62782e5
Maxime BOCHON <maxime.bochon@gmail.com>
2023-01-29 11:15:22 +0100

    Add alias to restart display manager

--- main-aliases.sh
+++ main-aliases.sh
@@ -1,3 +1,6 @@
+# Restart the display manager
+alias restartDisplayManager="sudo systemctl restart display-manager.service"
+
 # Get information on the process associated to a window by clicking on it
 alias getWindowProcessInformation='echo "Click on a window to get its process information"; ps -p $(xprop | awk '\''/PID/ {print $3}'\'') -o user,pid,args'
 
